bbd88ca117676fecad4018b32d48408cf020bc18
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2019-11-01 18:06:16 +0530

    Created variable model name

--- app/model/model.py
+++ app/model/model.py
@@ -4,9 +4,12 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.python.keras.backend import set_session
 
+MODEL_NAME = 'mnist.h5'
+
 model = None
 graph = None
 sess = None
+
 
 def init():
     global model
@@ -15,7 +18,7 @@ def init():
     print("Loading Model")
     print(os.path.dirname(os.path.abspath(__file__)))
     print(os.getcwd())
-    model_path = os.path.join(os.getcwd(), 'app','model', 'mnist.h5')
+    model_path = os.path.join(os.getcwd(), 'app','model', MODEL_NAME)
     sess = tf.Session()
     set_session(sess)
     m = keras.models.load_model(model_path)
